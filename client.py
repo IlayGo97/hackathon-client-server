c2f7a5f2828f261eeb594130b2a6a2efb3454e76
IlayGo97 <59761605+IlayGo97@users.noreply.github.com>
2021-12-27 13:49:02 +0200

    Rebasing

--- client.py
+++ client.py
@@ -1,38 +1,73 @@
+import scapy.all as scapy
+import struct
 import socket
 import threading
-import sys
 
-#Wait for incoming data from server
-#.decode is used to turn the message in bytes to a string
+# receives messages from the server and notifies if the server shutdown.
 def receive(socket, signal):
-    while signal:
-        try:
-            data = socket.recv(1024)
-            print(data.decode("utf-8"))
-        except:
-            print("You have been disconnected from the server")
-            signal = False
-            break
+    try:
+        while signal[0]:
+            try:
+                data = socket.recv(1024)
+                message = data.decode("utf-8")
+                if message == '':
+                    signal[0] = False
+                print(message)
+            except:
+                signal[0] = False
+                break
+    finally:
+        # the main thread is stuck on input() so we need to guide him...
+        print("Server disconnected, listening for offer requests...\npress enter to search for a new game.")
 
-#Get host and port
-host = input("Host: ")
-port = int(input("Port: "))
+# listens to the predetermined port for broadcasted invites.
+def look_for_game(my_ip: str):
+    invites_port = 13117
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.bind((my_ip, invites_port))
+    print("Client started, listening for offer requests...")
+    data, address = sock.recvfrom(512)
+    (HOST_IP, trash) = address
+    print("Received offer from {hostip}, attempting to connect...".format(hostip=HOST_IP))
+    (COOKIE, MESSAGE_TYPE, HOST_PORT) = struct.unpack('IbH', data)
+    sock.close()
+    return HOST_IP, HOST_PORT
 
-#Attempt connection to server
-try:
+
+def game_mode(host_address, team_name):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-except:
-    print("Could not make a connection to the server")
-    input("Press enter to quit")
-    sys.exit(0)
+    try:
+        sock.connect(host_address)
+    except:
+        print("TCP connection was declined.")
+        return
+    signal = [True]
+    receive_thread = threading.Thread(target=receive, args=(sock, signal))
+    receive_thread.start()
+    sock.sendall(str.encode(team_name))
+    # sends messages until servers shuts the connection on its end
+    while signal[0]:
+        message = input()
+        sock.sendall(str.encode(message))
+    print("Game shutdown.")
 
-#Create new thread to wait for data
-receiveThread = threading.Thread(target = receive, args = (sock, True))
-receiveThread.start()
 
-#Send data to server
-#str.encode is used to turn the string message into bytes so it can be sent across the network
-while True:
-    message = input()
-    sock.sendall(str.encode(message))
+def main():
+    print("What network do you want to use? (Ethernet 1 etc)")
+    inteface = input()
+    try:
+        my_ip = scapy.get_if_addr(inteface)
+    except:
+        print("Couldn't connect to " + inteface)
+        return
+    print("Enter a team name:")
+    team_name = input()
+    while True:
+        host_address = look_for_game(my_ip)
+        game_mode(host_address, team_name)
+        print("Searching for a new game.")
+
+
+if __name__ == '__main__':
+    main()
--- host.py
+++ host.py
@@ -4,35 +4,46 @@ import threading
 import time
 import select
 import random
+import struct
+
+# used for colored prompts
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 found_match = [False]
 
+# a function for sending invites over the predetermined port.
+# runs on a different thread.
 def invites(SERVER_PORT: int, MY_IP: str):
-    ENDIAN = 'little'
-    MAGICK_COOKIE = 0xabcddcba.to_bytes(4, ENDIAN)
-    MESSAGE_TYPE = 0x2.to_bytes(1, ENDIAN)
+    MAGIC_COOKIE = 0xabcddcba
+    MESSAGE_TYPE = 0x2
     INVITES_PORT = 13117
-    SERVER_PORT = SERVER_PORT.to_bytes(2, ENDIAN)
-    packet = MAGICK_COOKIE + MESSAGE_TYPE + SERVER_PORT
-
+    packet = struct.pack('IbH', MAGIC_COOKIE, MESSAGE_TYPE, SERVER_PORT)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind((MY_IP, 0))
-    try:
-        while not found_match[0]:
-            sock.sendto(packet, ("255.255.255.255", INVITES_PORT))
-            time.sleep(1)
-    finally:
-        sock.close()
-    pass
+    while not found_match[0]:
+        sock.sendto(packet, ("255.255.255.255", INVITES_PORT))
+        time.sleep(1)
+    sock.close()
+    return
 
+# wait for 2 clients.
 def wait_for_clients(ip_address):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((ip_address, 0))
-    invites_thread = threading.Thread(target= invites, args=(sock.getsockname()[1],ip_address,))
+    invites_thread = threading.Thread(target=invites, args=(sock.getsockname()[1], ip_address,))
     invites_thread.start()
     print("starting on port = {port}".format(port=sock.getsockname()[1]))
-
     number_of_players = 0
     player_sockets = []
     print("Server started, listening on IP {ip}".format(ip=ip_address))
@@ -41,17 +52,16 @@ def wait_for_clients(ip_address):
         (socket_player, player_IP) = sock.accept()
         player_sockets.append(socket_player)
         number_of_players = number_of_players + 1
-        print("Found player {player} !".format(player= number_of_players))
-    found_match[0] = True
+        print("Found player {player}!".format(player=number_of_players))
+    found_match[0] = True # to notify the invites thread it should die.
     sock.close()
     return player_sockets
 
+# simple question generation function.
 def generate_question():
-    answer = ""
-    scalar1 = random.randint(0, 9)
-    scalar2 = random.randint(0, 9)
+    scalar1 = random.randint(0, 3)
+    scalar2 = random.randint(0, 3)
     operator_r = random.randint(0, 2)
-    operator = ""
     if operator_r == 0:
         operator = " + "
         answer = str(scalar1 + scalar2)
@@ -66,57 +76,79 @@ def generate_question():
             temp = scalar1
             scalar1 = scalar2
             scalar2 = temp
-            answer = str(scalar2 - scalar1)
+            answer = str(scalar1 - scalar2)
     question = str(scalar1) + operator + str(scalar2)
     return question, answer
 
+# when starting a game session.
 def game_mode(player_sockets):
     player1_socket = player_sockets[0]
     player2_socket = player_sockets[1]
-    player1_name = player1_socket.recv(1024).decode()
-    player2_name = player2_socket.recv(1024).decode()
-    number_question, answer = generate_question()
-    welcome_string = "Welcome to Quick Maths.\nPlayer 1: {player1}\nPlayer 2: {player2}\n==".format(player1=player1_name, player2=player2_name)
-    question_string = "\nPlease answer the following question as fast as you can:\nHow much is {question}?".format(question= number_question)
-    timeout_string = "Connection timed-out, a draw."
-    player1_socket.send((welcome_string + question_string).encode())
-    player2_socket.send((welcome_string + question_string).encode())
-    first_player, x, y = select.select(player_sockets, [], player_sockets, 10)
-    if not first_player or len(first_player) == 0:
-        player2_socket.send(timeout_string.encode())
-        player1_socket.send(timeout_string.encode())
-        player1_socket.close()
-        player2_socket.close()
-        return
-    answering_sock = first_player[0]
-    winning_string = "The answer was {ans}!\n".format(ans=answer)
     try:
+        # receives team names.
+        player1_name = player1_socket.recv(1024).decode()
+        player2_name = player2_socket.recv(1024).decode()
+
+        number_question, answer = generate_question()
+        welcome_string = (bcolors.BLUE + "Welcome to Quick Maths." + bcolors.ENDC + bcolors.GREEN + "\nPlayer 1: {player1}" + bcolors.ENDC + bcolors.CYAN + "\nPlayer 2: {player2}" + bcolors.ENDC + "\n==").format(
+            player1=player1_name, player2=player2_name)
+        question_string = "\nPlease answer the following question as fast as you can:\nHow much is {question}?".format(
+            question=number_question)
+        question_string = bcolors.PURPLE + bcolors.BOLD + question_string + bcolors.ENDC
+        timeout_string = bcolors.RED + "Connection timed-out, a draw." + bcolors.ENDC
+        # sending welcome message and question to both players.
+        player1_socket.sendall((welcome_string + question_string).encode())
+        player2_socket.sendall((welcome_string + question_string).encode())
+        first_player, x, y = select.select(player_sockets, [], player_sockets, 10)
+        if not first_player or len(first_player) == 0:
+            player2_socket.sendall(timeout_string.encode())
+            player1_socket.sendall(timeout_string.encode())
+            player1_socket.shutdown(socket.SHUT_RDWR)
+            player2_socket.shutdown(socket.SHUT_RDWR)
+            return
+        answering_sock = first_player[0]
+        winning_string = "The answer was {ans}!\n".format(ans=answer)
         first_answer = answering_sock.recv(512).decode("utf-8")
         first_answer = first_answer.split()[0]
-        print("received answer! {answer}".format(answer= first_answer))
+        print("received answer! {answer}".format(answer=first_answer))
         if first_answer == answer and answering_sock == player1_socket:
-            winning_string = winning_string + "TEAM {team_name} WINS, FATALITY!".format(team_name= player1_name)
+            winning_string = winning_string + "TEAM {team_name} WINS, FATALITY!".format(team_name=player1_name)
         elif answering_sock == player1_socket:
-            winning_string = winning_string + "TEAM {team_name} WINS, PERFECT!".format(team_name= player2_name)
+            winning_string = winning_string + "TEAM {team_name} WINS, PERFECT!".format(team_name=player2_name)
         elif first_answer == answer and answering_sock == player2_socket:
-            winning_string = winning_string + "TEAM {team_name} WINS, THEY ARE UNBEATABLE (in the last 1 matches)!".format(team_name= player2_name)
+            winning_string = winning_string + "TEAM {team_name} WINS, THEY ARE UNBEATABLE (in the last 1 matches)!".format(
+                team_name=player2_name)
         elif answering_sock == player2_socket:
-            winning_string = winning_string + "TEAM {team_name} WINS, AMAZING PERFORMANCE!".format(team_name= player1_name)
-        player1_socket.send(winning_string.encode("utf-8"))
-        player2_socket.send(winning_string.encode("utf-8"))
+            winning_string = winning_string + "TEAM {team_name} WINS, AMAZING PERFORMANCE!".format(
+                team_name=player1_name)
+        winning_string = bcolors.YELLOW + winning_string + bcolors.ENDC
+        player1_socket.sendall(winning_string.encode())
+        player2_socket.sendall(winning_string.encode())
+    except:
+        print("Connection error.")
     finally:
         player1_socket.shutdown(socket.SHUT_RDWR)
         player2_socket.shutdown(socket.SHUT_RDWR)
-        print("game is over.")
+        print("Game over, sending out offer requests...")
 
 
 def main():
+    # for lab purposes.
+    print("What network do you want to use? (Ethernet 1 etc)")
+    inteface = input()
+    try:
+        my_ip = scapy.get_if_addr(inteface)
+    except:
+        print("Couldn't connect to "+inteface)
+        return
     while True:
         found_match[0] = False
-        players = wait_for_clients("127.0.0.1")
+        players = wait_for_clients(my_ip)
+        my_ip = my_ip.split()[0]
         print("a new game starts in 10 seconds!")
         time.sleep(10)
         game_mode(players)
+
 
 if __name__ == '__main__':
     main()
